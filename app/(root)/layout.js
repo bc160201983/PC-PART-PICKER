e085f448c5e20551353a700689efaf88a8c6d3c1
Muhammad Bilal <softnacuk@gmail.com>
2023-11-01 15:19:36 +0000

    footer added

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import Navbar from "@/components/shared/Navbar/Navbar";
+import Footer from "@/components/shared/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/shared/footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="h-[400px] flex flex-col justify-center items-center">
+      <div className="wrapper h-full max-w-6xl">
+        <div className="logo">PCPARTPICKER</div>
+        <div className="cardLink">
+          <h1>PCPP</h1>
+          <ul>
+            <li>Builder</li>
+            <li>Products</li>
+            <li>Guides</li>
+            <li>Completed Builds</li>
+            <li>Trends</li>
+            <li>Forums</li>
+          </ul>
+        </div>
+        <div className="cardLink">
+          <h1>PCPP</h1>
+          <ul>
+            <li>Builder</li>
+            <li>Products</li>
+            <li>Guides</li>
+            <li>Completed Builds</li>
+            <li>Trends</li>
+            <li>Forums</li>
+          </ul>
+        </div>
+        <div className="cardLink">
+          <h1>PCPP</h1>
+          <ul>
+            <li>Builder</li>
+            <li>Products</li>
+            <li>Guides</li>
+            <li>Completed Builds</li>
+            <li>Trends</li>
+            <li>Forums</li>
+          </ul>
+        </div>
+      </div>
+      <div className="foorterInfo mt-6 border-t h-1/4 border-black w-full max-w-6xl flex justify-center items-center pt-6">
+        ©2023 PCPartPicker, LLC. All rights reserved.
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
